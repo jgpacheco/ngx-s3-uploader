3f6a8a7c0c5397b067d813f2ee4e69b02d39d28c
Jaime <jaime.gez.pacheco@gmail.com>
2017-11-30 02:39:16 -0500

    Amazon cognito identity support added

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [0.4.0] - 2017-11-29
+### Fixed
+- OpaqueToken was removed in Angular 5. Use InjectionToken instead.
+
+### Added
+- Amazon cognito identity support added.
+
--- src/s3-uploader.service.ts
+++ src/s3-uploader.service.ts
@@ -1,33 +1,54 @@
-import { Inject, Injectable, OpaqueToken } from '@angular/core';
+import { Inject, Injectable, InjectionToken } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { S3 } from 'aws-sdk';
+import { S3, CognitoIdentityCredentials, config } from 'aws-sdk';
 
 export interface S3UploaderConfig {
     apiVersion?: string;
     region: string;
     bucket: string;
-    credentials: { accessKeyId: string, secretAccessKey: string };
+    credentials: { accessKeyId?: string, secretAccessKey?: string, identityPoolId?: string, logins?: any };
 }
 
-export const CONFIG = new OpaqueToken('config');
+export const CONFIG = new InjectionToken<S3UploaderConfig>('config');
 
 @Injectable()
 export class S3UploaderService {
 
     private client: S3;
 
-    constructor(@Inject(CONFIG) private config: S3UploaderConfig) {
-        this.client = new S3({
-            apiVersion: config.apiVersion || '2006-03-01',
-            region: config.region,
-            credentials: config.credentials,
-        });
+    constructor(@Inject(CONFIG) private s3UploaderConfig: S3UploaderConfig) {
+        let credentials = {
+            accessKeyId: s3UploaderConfig.credentials.accessKeyId,
+            secretAccessKey: s3UploaderConfig.credentials.secretAccessKey,
+        };
+
+        if (s3UploaderConfig.credentials.identityPoolId) {
+            credentials = new CognitoIdentityCredentials({
+                IdentityPoolId: s3UploaderConfig.credentials.identityPoolId,
+                Logins: this.s3UploaderConfig.credentials.logins || {},
+            });
+        }
+
+        config.region = s3UploaderConfig.region;
+        config.credentials = credentials;
+
+        this.client = new S3();
+    }
+
+    public  authenticate(providerName: string, token: string): void {
+        if (config.credentials instanceof CognitoIdentityCredentials) {
+            config.credentials['params']['Logins'] = {};
+            config.credentials['params']['Logins'][providerName] = token;
+
+            // Expire credentials to refresh them on the next request.
+            config.credentials['expired'] = true;
+        }
     }
 
     public upload(file: File, acl = 'public-read', bucket?: string): Observable<any> {
         return Observable.create((observer) => {
             this.client.upload(
-                { Key: file.name, Body: file, ACL: acl, ContentType: file.type, Bucket: bucket || this.config.bucket },
+                { Key: file.name, Body: file, ACL: acl, ContentType: file.type, Bucket: bucket || this.s3UploaderConfig.bucket },
                 (error, data) => {
                     if (error) { return observer.error(error); }
 
