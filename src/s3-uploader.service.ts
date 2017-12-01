33933c5e6f5520292f4663c1416c8a04b043e619
Emanuele Tonello <emanueletonello@gmail.com>
2017-12-01 17:19:34 +0100

    Added support for web federated identity (#6)

--- src/s3-uploader.service.ts
+++ src/s3-uploader.service.ts
@@ -1,12 +1,21 @@
 import { Inject, Injectable, InjectionToken } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { S3, CognitoIdentityCredentials, config } from 'aws-sdk';
+import { S3, CognitoIdentityCredentials, WebIdentityCredentials, config } from 'aws-sdk';
 
 export interface S3UploaderConfig {
-    apiVersion?: string;
-    region: string;
-    bucket: string;
-    credentials: { accessKeyId?: string, secretAccessKey?: string, identityPoolId?: string, logins?: any };
+  apiVersion?: string;
+  region: string;
+  bucket: string;
+  credentials: {
+    accessKeyId?: string,
+    secretAccessKey?: string,
+    identityPoolId?: string,
+    logins?: any,
+    roleArn?: string,
+    roleName?: string,
+    providerId?: string,
+    token?: string,
+  };
 }
 
 export const CONFIG = new InjectionToken<S3UploaderConfig>('config');
@@ -14,48 +23,59 @@ export const CONFIG = new InjectionToken<S3UploaderConfig>('config');
 @Injectable()
 export class S3UploaderService {
 
-    private client: S3;
+  private client: S3;
 
-    constructor(@Inject(CONFIG) private s3UploaderConfig: S3UploaderConfig) {
-        let credentials = {
-            accessKeyId: s3UploaderConfig.credentials.accessKeyId,
-            secretAccessKey: s3UploaderConfig.credentials.secretAccessKey,
-        };
+  constructor( @Inject(CONFIG) private s3UploaderConfig: S3UploaderConfig) {
+    let credentials = {
+      accessKeyId: s3UploaderConfig.credentials.accessKeyId,
+      secretAccessKey: s3UploaderConfig.credentials.secretAccessKey,
+    };
 
-        if (s3UploaderConfig.credentials.identityPoolId) {
-            credentials = new CognitoIdentityCredentials({
-                IdentityPoolId: s3UploaderConfig.credentials.identityPoolId,
-                Logins: this.s3UploaderConfig.credentials.logins || {},
-            });
-        }
-
-        config.region = s3UploaderConfig.region;
-        config.credentials = credentials;
-
-        this.client = new S3();
+    // if the identity pool id  is populated I use the Cognito credentials to authenticate
+    if (s3UploaderConfig.credentials.identityPoolId) {
+      credentials = new CognitoIdentityCredentials({
+        IdentityPoolId: s3UploaderConfig.credentials.identityPoolId,
+        Logins: this.s3UploaderConfig.credentials.logins || {},
+      });
     }
 
-    public  authenticate(providerName: string, token: string): void {
-        if (config.credentials instanceof CognitoIdentityCredentials) {
-            config.credentials['params']['Logins'] = {};
-            config.credentials['params']['Logins'][providerName] = token;
-
-            // Expire credentials to refresh them on the next request.
-            config.credentials['expired'] = true;
-        }
+    // if role is populated I use the Web Federated Identity to authenticate
+    if (s3UploaderConfig.credentials.roleArn) {
+      credentials = new WebIdentityCredentials({
+        RoleArn: s3UploaderConfig.credentials.roleArn,
+        RoleSessionName: s3UploaderConfig.credentials.roleName,
+        ProviderId: this.s3UploaderConfig.credentials.providerId,
+        WebIdentityToken: this.s3UploaderConfig.credentials.token
+      });
     }
 
-    public upload(file: File, acl = 'public-read', bucket?: string): Observable<any> {
-        return Observable.create((observer) => {
-            this.client.upload(
-                { Key: file.name, Body: file, ACL: acl, ContentType: file.type, Bucket: bucket || this.s3UploaderConfig.bucket },
-                (error, data) => {
-                    if (error) { return observer.error(error); }
+    config.region = s3UploaderConfig.region;
+    config.credentials = credentials;
 
-                    observer.next(data);
-                    observer.complete();
-                });
+    this.client = new S3();
+  }
+
+  // Authenticate with Cognito credentials
+  public authenticate(providerName: string, token: string): void {
+    if (config.credentials instanceof CognitoIdentityCredentials) {
+      config.credentials['params']['Logins'] = {};
+      config.credentials['params']['Logins'][providerName] = token;
+
+      // Expire credentials to refresh them on the next request.
+      config.credentials['expired'] = true;
+    }
+  }
+
+  public upload(file: File, acl = 'public-read', bucket?: string): Observable<any> {
+    return Observable.create((observer) => {
+      this.client.upload(
+        { Key: file.name, Body: file, ACL: acl, ContentType: file.type, Bucket: bucket || this.s3UploaderConfig.bucket },
+        (error, data) => {
+          if (error) { return observer.error(error); }
+
+          observer.next(data);
+          observer.complete();
         });
-    }
-
+    });
+  }
 }
